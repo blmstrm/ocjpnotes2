e9b4b86d90a2b45057c2d60af14b5554fd43a820
Kalle Blomström <kalle.blomstrom@gmail.com>
2013-04-05 11:52:18 +0200

    Added singleton implementation

--- com/blmstrm/ocjp2/plates/EverydayPlate.java
+++ com/blmstrm/ocjp2/plates/EverydayPlate.java
@@ -5,22 +5,23 @@ import com.blmstrm.ocjp2.meals.Meal;
 public enum EverydayPlate implements Plate {
 
 	INSTANCE;
-	
+
 	private Meal plateContent;
-		
+
 	public void plateFood(Meal food){
 		this.plateContent = food;
 		System.out.println("Put "+plateContent.getType()+" on plate.");
 	}
-	
+
 	public Meal getPlateContent(){
 		return plateContent;
 	}
 
 	public void clean() {
-	this.plateContent = null;	
-	System.out.println("Cleaned plate.");
+		this.plateContent = null;	
+		System.out.println("Cleaned plate.");
 	}
+
 	@Override
 	public String toString(){
 		return "There is "+this.plateContent.getType()+" on the plate.";
