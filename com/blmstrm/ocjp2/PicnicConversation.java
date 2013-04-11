ca3bce6cb4d4b9d8dae9bc6cff4265b49a345df9
Kalle Blomström <kalle.blomstrom@gmail.com>
2013-04-11 17:36:12 +0200

    Adds drogba/anka switch

--- com/blmstrm/ocjp2/PicnicConversation.java
+++ com/blmstrm/ocjp2/PicnicConversation.java
@@ -4,31 +4,52 @@ import java.io.Console;
 
 public class PicnicConversation {
 
+	private Talker currentTalker;
+
+	private enum Talker {
+		DROGBA,ANKA
+	}
 
 	private Console console;
 
 	{
-		console =System.console();
+
+		console = System.console();
 		console.printf("Opening file\n");
+		this.currentTalker = Talker.DROGBA;
 		/*Open file conversation*/
-		
-		
+
 	}
 
 	public static void main(String args[]){
-		
+
 		PicnicConversation convo = new PicnicConversation();
 		convo.talk();
 	}
 
 	private void talk(){
+		String line = "";
 
-		try {
-			
-	
-		} finally{
-			console.printf("Saving file\n");
+		while(true){
+			switch(currentTalker){
+			case DROGBA:
+				console.printf("Drogba: ");
+				line +="Drogba: ";
+				currentTalker=Talker.ANKA;
+				break;
+			case ANKA:
+				console.printf("Anka: ");
+				line +="Anka: ";
+				currentTalker=Talker.DROGBA;
+				break;
+			}
+		
+			line += console.readLine();
+			line += "\n";
+			line ="";
 		}
+
+
 	}
-	
+
 }
